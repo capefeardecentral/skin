5e441c309ee2ea1fbd17b0933cd7135d30a46174
Cape Fear Decentral <info@capefeardecentral.net>
2023-02-19 16:14:01 -0500

    continue work on consensus logic

--- contracts/contracts/Consensus.sol
+++ contracts/contracts/Consensus.sol
@@ -9,13 +9,12 @@ contract Consensus is OrderBook {
 
     IERC20 public skinToken;
     mapping (address => mapping(Tokens => uint)) public votes;
-    uint256 public totalVotes;
     uint256 public yesVotes;
     uint256 public noVotes;
     uint256 public voteStartTime;
     uint256 public voteEndTime;
     uint256 public reward;
-    Tokens public winner = Tokens.NONE;
+    Tokens public consensus_winner = Tokens.NONE;
 
     constructor(address _skinToken, uint _voteDuration) {
         skinToken = IERC20(_skinToken);
@@ -30,7 +29,6 @@ contract Consensus is OrderBook {
 
         skinToken.transferFrom(msg.sender, address(this), _amount);
         votes[msg.sender][_token] += _amount;
-        totalVotes += _amount;
         if (_token == Tokens.YES) {
             yesVotes += _amount;
         } else {
@@ -41,26 +39,56 @@ contract Consensus is OrderBook {
 
     function settle() external {
         require(block.timestamp >= voteEndTime, "Consensus: Voting has not ended");
-        require(winner == Tokens.NONE, "Consensus: vote is already settled");
+        require(consensus_winner == Tokens.NONE, "Consensus: vote is already settled");
         Tokens consensusResult = yesVotes > noVotes ? Tokens.YES : Tokens.NO;
-        winner = consensusResult == Tokens.YES ? Tokens.YES : Tokens.NO;
-        reward = winner == Tokens.YES ? get_reward(yesVotes, noVotes) : get_reward(noVotes, yesVotes);
+        bool consensusReached = _get_consensus_reached(yesVotes, noVotes);
+        if (consensusReached) {
+            consensus_winner = consensusResult == Tokens.YES ? Tokens.YES : Tokens.NO;
+            reward = consensus_winner == Tokens.YES ? _get_reward(yesVotes, noVotes) : _get_reward(noVotes, yesVotes);
+        } else {
+            // fail with error
+            revert("Consensus: Consensus not reached");
+
+        }
     }
 
     function claim() external {
         require(block.timestamp >= voteEndTime, "Consensus: Voting has not ended");
-        require(winner != Tokens.NONE, "Consensus: Consensus has not been reached");
-        uint256 _reward = votes[msg.sender][winner] * reward;
+        require(consensus_winner != Tokens.NONE, "Consensus: Consensus has not been reached");
+        uint256 _reward = votes[msg.sender][consensus_winner] * reward;
         skinToken.transfer(msg.sender, _reward);
-        delete votes[msg.sender][winner];
+        delete votes[msg.sender][consensus_winner];
     }
 
-    function get_reward(uint _winVotes, uint _loseVotes) internal pure returns (uint) {
+    function _get_consensus_reached(uint _winVotes, uint_loseVotes) private pure returns (bool) {
+        // no one voted for consensus
+        if (_winVotes == 0) {
+            return false;
+        }
+
+        // consensus was unanimous
+        if (_loseVotes == 0) {
+            return true;
+        }
+
+        // set 66% threshold for consensus
+        return _winVotes >= (_loseVotes * 3) ? true : false;
+    }
+
+    function _get_reward(uint _winVotes, uint _loseVotes) private pure returns (uint) {
         if (_loseVotes == 0) {
             return 0;
         }
 
         uint _reward = _winVotes / _loseVotes;
         return _reward;
+    }
+
+    // let's extend consensus if we can not defer to orderbook for a winner
+    // this would likely happen if an outcome is not reached by the originally set voteEndTime
+    function _extend_consensus(uint _duration) internal {
+        require(block.timestamp >= voteEndTime, "Consensus: Voting has not ended");
+        require(consensus_winner == Tokens.NONE, "Consensus: vote is already settled");
+        voteEndTime = block.timestamp + _duration;
     }
 }
